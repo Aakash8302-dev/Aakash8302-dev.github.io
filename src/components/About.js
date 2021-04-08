4cc1137d99fce68725ccd2ca2d2ecdd6bdf84088
Aakash arumugam <aakasharumugam18@gmail.com>
2021-04-08 10:09:53 +0530

    Latest resume

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
                     <h4 id="about-title"><i class="fas about-icon fa-user"></i>   About Me</h4>
                     <p className="about-p">I am Aakash Arumugam a Ist year student currently pursuing a Bachelor in Information Technology at Sri Venkateswara College of Engineering, Chennai.</p>
                     
-                    <a  target="_blank" rel="noreferrer noopener" href="https://drive.google.com/file/d/1xsin4RnsJbmNENLKLz_K9V7ilmVpl7hX/view?usp=sharing">
+                    <a  target="_blank" rel="noreferrer noopener" href="https://drive.google.com/file/d/1t9Ih_b3Fqh7kgDNNi85a_HjXNSGBUN9n/view?usp=sharing">
                         <button className="resume-btn" type="button">
                             Get my Resume
                         </button>
